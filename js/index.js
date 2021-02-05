e2631ecc094244c3bc11b4442a6bed82f03316be
Stelian <69723096+stelko0@users.noreply.github.com>
2021-02-06 00:15:48 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+    x.classList.toggle("change-menu");
+  };
+
+
+function toggleForm() {
+  var container = document.querySelector(".containerr");
+  container.classList.toggle("active");
+};
+
+
